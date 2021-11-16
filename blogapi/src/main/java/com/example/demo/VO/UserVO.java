bea39775a6d45c80b33b85d3f6778bf71b8a345d
junghyun.ahn <shiki13@naver.com>
2021-11-16 17:17:09 +0900

    test111

--- blogapi/src/main/java/com/example/demo/VO/UserVO.java
+++ blogapi/src/main/java/com/example/demo/VO/UserVO.java
@@ -1,19 +1,89 @@
 package com.example.demo.VO;
 
 public class UserVO extends CommonVO {
-	private String member_info_seq;
-	private String member_id;
-	public String getMember_info_seq() {
-		return member_info_seq;
+	private String userSeq;
+	private String userId;
+	private String userPwd;
+	private String userToken;
+	private String authCode;
+	private String useYn;
+	private String firstLoginYn;
+	private String url;
+	
+	/**
+	 * @return the firstLoginYn
+	 */
+	public String getFirstLoginYn() {
+		return firstLoginYn;
 	}
-	public void setMember_info_seq(String member_info_seq) {
-		this.member_info_seq = member_info_seq;
+
+	/**
+	 * @param firstLoginYn the firstLoginYn to set
+	 */
+	public void setFirstLoginYn(String firstLoginYn) {
+		this.firstLoginYn = firstLoginYn;
 	}
-	public String getMember_id() {
-		return member_id;
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
 	}
-	public void setMember_id(String member_id) {
-		this.member_id = member_id;
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUserSeq() {
+		return userSeq;
+	}
+
+	public void setUserSeq(String userSeq) {
+		this.userSeq = userSeq;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserPwd() {
+		return userPwd;
+	}
+
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+
+	public String getUserToken() {
+		return userToken;
+	}
+
+	public void setUserToken(String userToken) {
+		this.userToken = userToken;
+	}
+
+	public String getAuthCode() {
+		return authCode;
+	}
+
+	public void setAuthCode(String authCode) {
+		this.authCode = authCode;
+	}
+
+	public String getUseYn() {
+		return useYn;
+	}
+
+	public void setUseYn(String useYn) {
+		this.useYn = useYn;
 	}
 
 }
